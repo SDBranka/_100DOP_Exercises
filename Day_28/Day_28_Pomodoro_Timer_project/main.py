a6b8c03d5d8e1ddb2128d19d624c826779751a06
Sam Branka <sdbranka@me.com>
2022-05-28 06:29:18 -0400

    Day 28 complete

--- Day_28/Day_28_Pomodoro_Timer_project/main.py
+++ Day_28/Day_28_Pomodoro_Timer_project/main.py
@@ -14,30 +14,69 @@ GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
 WORK_MIN = 25
+# WORK_MIN = .1
 SHORT_BREAK_MIN = 5
+# SHORT_BREAK_MIN = .1
 LONG_BREAK_MIN = 20
+# LONG_BREAK_MIN = .1
+reps = 0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    timer_label.config(text="Timer")
+    check_marks.config(text="")
+    global reps
+    reps = 0
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    count_down(5)
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        timer_label.config(text="Long Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        timer_label.config(text="Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        timer_label.config(text="Work", fg=GREEN)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
     # print(count)
     count_minutes = math.floor(count / 60)
     count_seconds = count % 60
+    # python dynamic typing (a variable can hold changeable data types ex:a = 3 a = "kkk")
+    if count_seconds < 10:
+        count_seconds = f"0{count_seconds}"
 
-
-    canvas.itemconfig(f"{count_minutes}:{count_seconds}", text=count)
+    canvas.itemconfig(timer_text, text=f"{count_minutes}:{count_seconds}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        marks = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            marks += "✔"
+        check_marks.config(text = marks)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = tkinter.Tk()
 window.title("Pomodoro Timer")
 window.config(padx = 108, pady = 54, bg = YELLOW)
+
 
 # canvas
 # so that we can use an image in the window
@@ -50,11 +89,7 @@ timer_text = canvas.create_text(100, 135, text = "00:00",
                     fill = "white", 
                     font = (FONT_NAME, 35, "bold")
 )
-# canvas.pack()
 canvas.grid(row = 1, column = 1)
-
-
-
 
 #"Timer" label
 timer_label = tkinter.Label(text = "Timer", 
@@ -65,6 +100,7 @@ timer_label.grid(row = 0, column = 1)
 
 # start button
 start_button = tkinter.Button(text = "Start",
+                                highlightthickness = 0,
                                 command = start_timer,
                                 font = ("Arial", 18, "normal"), 
                                 fg = "white", bg = "blue"
@@ -73,6 +109,8 @@ start_button.grid(row = 2, column = 0)
 
 # reset button
 reset_button = tkinter.Button(text = "Reset",
+                                highlightthickness = 0,
+                                command = reset_timer,
                                 font = ("Arial", 18, "normal"),
                                 fg = "white", bg = "blue"
 )
